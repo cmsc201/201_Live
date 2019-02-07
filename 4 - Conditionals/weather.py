40d7106b6c93c8afee3bd1bb9f8335997bb5e4c2
Ben <doc.ben.johnson@gmail.com>
2019-02-06 19:03:39 -0500

    mmm turnips

--- 4 - Conditionals/weather.py
+++ 4 - Conditionals/weather.py
@@ -1,5 +1,25 @@
 def main():
     is_hot = input("Hi!  Is it hot out?")
+    if is_hot.lower() == "yes":
+        is_raining = input("Is it raining?")
+        if is_raining == "yes":
+            print("Take an umbrella!")
+        elif is_raining == "no":
+            print("Something light, perhaps a shawl.")
+    else:
+        we_cool = input("Hey.  Hey.  We cool?")
+        if we_cool == "yes":
+            print("cool")
+        else:
+            something = input("What is your favorite food?")
+            if something == "pizza":
+                print("How original.  What if it's on a bagel??")
+            if something == "roasted turnips with polenta":
+                print("Wow, I don't think I'm fancy enough for you.")
+            else:
+                print("Oh.  Cool.")
+
 
 
 main()
+
