1a8cbd78d5145eb91fe7b4e7ee4a62ccf392b3f6
Ben <doc.ben.johnson@gmail.com>
2019-02-14 10:59:15 -0500

    loops, part 1

---
+++ Looping/loops.py
@@ -0,0 +1,32 @@
+# guess = 0
+# while guess != 4:
+#     guess = int(input("Guess!"))
+#
+# print("Yay!  Good job!")
+
+
+# #I want the user to put in numbers and then I print their sum,
+# number = int(input("NUMBER!"))
+# total = 0
+# while number != 0:
+#     number += int(input("NUMBER!"))
+#     total += number
+#
+# print("It's this!", total)
+
+# product = 1
+# operand = 1
+# while operand <= 6:
+#     product *= operand
+#     operand += 1
+#
+# print(product)
+
+number = 0
+while number <= 100:
+    if number % 3 == 0:
+        print(number)
+    number += 1
+
+
+
