17fa4d2917ee3cfc5c036af31ecc93d405fd1f49
Ben <doc.ben.johnson@gmail.com>
2019-02-17 21:21:15 -0500

    woops

---
+++ 5 - Looping/live_loops1.py
@@ -0,0 +1,43 @@
+# have the user input values until they input 4
+# number = 0
+# while number < 1970:
+#     number = int(input("Input a year after 1970:"))
+# print("Oh you...")
+
+# have the user input values and print the sum after they input 0
+# number = int(input("Hey.  Wanna give me a number?"))
+# sum = number
+# while number != 0:
+#     number = int(input("How about another?"))
+#     sum += number
+#
+# print(sum)
+
+
+# compute 10!
+# operand = 1
+# goal = int(input("What factorial you want?"))
+# product = 1
+# while operand <= goal:
+#     product *= operand
+#     operand += 1
+#
+# print(product)
+
+# print even numbers between 0 and 100, inclusive
+# number = 0
+# while number < 100:
+#     if number % 2 == 0:
+#         print(number)
+#     if number < 100:
+#         number += 1
+#
+# print("secretly it's", number)
+
+# print odd numbers between 0 and 100, inclusive
+number = 1
+while number != 100:
+    print(number)
+    number += 2
+
+print("secretly it's", number)
