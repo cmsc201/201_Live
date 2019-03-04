c2ce0270ddef042c524b3736a00914f8450b87fb
Ben <doc.ben.johnson@gmail.com>
2019-03-04 15:47:32 -0500

    functions

---
+++ 10 - Functions/live.py
@@ -0,0 +1,13 @@
+# Write a program that:
+# 1. Asks the user whether they want to (1) add (2) subtract or (3) both
+# 2. Write a function for add and subtract
+
+
+# Ask a user for a number, and then offer to square it, square root it, or multiply by pi.
+
+# Write a function that prints out a list, each item on its own line.
+
+# Modify the grocery app from last class to use that list function.
+
+# Write a function that asks the user for their age until they give something between 0 and 150.  Wait.  Can we do this?
+
---
+++ README.md
@@ -0,0 +1,7 @@
+## Live coding from cs201 taught by Dr. Ben Johnson
+
+Hello internet traveler!  This repository contains the wit and wisdom of UMBC cs201 students.
+
+### Where is the code?
+
+There is a branches drop down at the top.  `master` contains the blank starting points, and all the other branches are for different sections.
