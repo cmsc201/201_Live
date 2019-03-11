a701649c35ae995e2b3c80a8e970b341a13906fd
Ben <doc.ben.johnson@gmail.com>
2019-03-11 14:09:15 -0400

    functions part 2

---
+++ 10 - Functions/part2.py
@@ -0,0 +1,18 @@
+# playing with functions!
+# square function that returns result
+
+# slide example
+
+# def getExclamation(age):
+# 	if age > 18:
+# 		return "My word!"
+# 	print("Whoa it’s a kiddo")
+# 	return "Gee whiz!"
+
+# example where None is returned even though there is a return statement
+
+# example where we try to modify the parameters of a function
+
+# add two numbers and return the sum
+
+# return the sum of some numbers in a list
