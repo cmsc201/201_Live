5288846e057b999c4c89102e525c55dcd7c212b1
Ben <doc.ben.johnson@gmail.com>
2019-03-19 17:29:12 -0400

    accessing list

---
+++ 13. More Lists/live.py
@@ -0,0 +1,12 @@
+matrix = [[1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 1, 2, 3],
+          [4, 5, 6, 7]]
+
+def main():
+    # how do I get at the 9?
+    # how do I loop through the third row?
+    # how do I loop through the second column?
+    pass
+
+main()
