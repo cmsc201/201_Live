e93a8b35602882874a038a5705ec0fe422e34cc1
Ben <doc.ben.johnson@gmail.com>
2019-03-27 22:05:28 -0400

    mw

--- 13. More Lists/fun_with_copying.py
+++ 13. More Lists/fun_with_copying.py
@@ -1,8 +1,19 @@
+def add_bird(bird_list, bird):
+    bird_list.append(bird)
+
+
 def main():
     doc_js_favorite_birds = ["kingfisher", "peregrine", "common swift", "frogmouth"]
-    your_favorite_birds = doc_js_favorite_birds
+    your_favorite_birds = []
+
+    # deep list copy
+    index = 0
+    while index < len(doc_js_favorite_birds):
+        your_favorite_birds.append(doc_js_favorite_birds[index])
+        index += 1
 
     print("Oh hey, I love starlings.")
+    add_bird(your_favorite_birds, "ducks")
     your_favorite_birds.append("starlings")
 
     print("Doc J, do you like starlings")
@@ -11,5 +22,6 @@ def main():
     else:
         print("Whoa, starlings? Gross!  They are fine where they came from, but they are meanies here in the US.")
 
+    print(your_favorite_birds)
 
 main()
--- 13. More Lists/live.py
+++ 13. More Lists/live.py
@@ -1,18 +1,66 @@
 matrix = [[1, 2, 3, 4],
           [5, 6, 7, 8],
           [9, 1, 2, 3],
-          [4, 5, 6, 7]]
+          [4, 5, 6, 7],
+          [8, 9, 1, 2]]
 
 
 def create_new_2d_list(height, width):
-    pass
+    row_index = 0
+    two_dee_list = []
+    index = 0
 
+    while row_index < height:
+        col_index = 0
+        row = []
+        two_dee_list.append(row)
+        while col_index < width:
+            row.append(index)
+            index += 1
+            col_index += 1
+        row_index += 1
+
+    return two_dee_list
+
+def make_square_list(dimension):
+    row_index = 0
+    two_dee_list = []
+    index = 0
+
+    while row_index < dimension:
+        col_index = 0
+        row = []
+        two_dee_list.append(row)
+        while col_index < dimension:
+            row.append(index)
+            index += 1
+            col_index += 1
+        row_index += 1
+
+    return two_dee_list
 
 def main():
     # how do I get at the 9?
+    print("Should be a nine:", matrix[2][0])
     # how do I loop through the third row?
+    index = 0
+    while index < 4:
+        print(matrix[2][index])
+        index += 1
+
     # how do I loop through the second column?
-    pass
+    index = 0
+    while index < 4:
+        print(matrix[index][2])
+        index += 1
+
+    # write a loop to make a 100x100 list
+
+
+    print(make_square_list(100))
+    print(make_square_list(10))
+    print(make_square_list(1))
+
 
 
 main()
