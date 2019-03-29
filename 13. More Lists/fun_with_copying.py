880e5faf6f444aec39bcf7805c29923ad765c0b3
Ben <doc.ben.johnson@gmail.com>
2019-03-29 12:35:27 -0400

    tuth

--- 13. More Lists/fun_with_copying.py
+++ 13. More Lists/fun_with_copying.py
@@ -3,11 +3,17 @@ def main():
     your_favorite_birds = doc_js_favorite_birds
 
     print("Oh hey, I love starlings.")
-    your_favorite_birds.append("starlings")
+    # shallow copy BAD -> your_favorite_birds.append("starlings")
+    your_favorite_birds = []
+    index = 0
+    while index < len(doc_js_favorite_birds):
+        your_favorite_birds.append(doc_js_favorite_birds[index])
+        index += 1
+
 
     print("Doc J, do you like starlings")
     if "starlings" in doc_js_favorite_birds:
-        print("Why yes I do! They are totally not nest-wrecking jerks and very much belong here.")
+        print("LIE: Why yes I do! They are totally not nest-wrecking jerks and very much belong here.")
     else:
         print("Whoa, starlings? Gross!  They are fine where they came from, but they are meanies here in the US.")
 
--- 13. More Lists/function_weirdness.py
+++ 13. More Lists/function_weirdness.py
@@ -1,15 +1,17 @@
-def increment_list(a):
-    index = 0
-    while index < len(a):
-        a[index] += 1
-        index += 1
-    return a
 
-# mutable function parameter example
+def add_to_list(a, item_to_add):
+    output = []
+    for thing in a:
+        output.append(thing)
+
+    output.append(item_to_add)
+    return output
+
+
 def main():
     a = [1, 2, 3, 4]
-    b = increment_list(a)
-    print(a, b)  # <- should be different
+    print(add_to_list(a, 5))
+    print(a)
 
 
 main()
