fe115c4cb160179ddbe149bd20cfc13aae3c0be4
Ben <doc.ben.johnson@gmail.com>
2019-04-01 14:30:43 -0400

    for loops

---
+++ 14 - For loops/live.py
@@ -0,0 +1,10 @@
+# How can we print the even numbers between two integers x and y with a for loop?
+# How can we use range to go backwards through a list?
+# What lines of code does a for loop write “for” us?
+# Print all the elements of a list with their index!  How do?
+# Print all the elements of a 2d list with their row and column!  How do?
+matrix = [[1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 1, 2, 3],
+          [4, 5, 6, 7]]
+
