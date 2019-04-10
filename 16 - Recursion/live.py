bd543d56995f5bf08edd62c9f8f3a3a5f88e28c2
Ben <doc.ben.johnson@gmail.com>
2019-04-10 17:17:34 -0400

    more recursion

--- 16 - Recursion/live.py
+++ 16 - Recursion/live.py
@@ -14,20 +14,52 @@
 
 # factorial(number) is factorial(number-1) * number OR
 # factorial(1) and factorial(0) = 1
-def factorial(number):
-    if number == 1 or number == 0:
-        return 1
-    else:
-        what_my_clone_did = factorial(number - 1)
-        return what_my_clone_did * number
-
-def main():
-    print(factorial(10))
-
-main()
+# def factorial(number):
+#     if number == 1 or number == 0:
+#         return 1
+#     else:
+#         what_my_clone_did = factorial(number - 1)
+#         return what_my_clone_did * number
+# def main():
+#     print(factorial(999))
+#
+# main()
+#
+# def fib(n):
+#     first_term = 1
+#     second_term = 1
+#     print(first_term)
+#     print(second_term)
+#     for i in range(n-2):
+#         temp = second_term
+#         second_term = first_term + second_term
+#         first_term = temp
+#         print(second_term)
+#
+# def fib_rec(n):
+#     print(n)
+#     if n == 1 or n == 2:
+#         return 1
+#     return fib_rec(n-1) + fib_rec(n-2)
+#
+# def main():
+#     print(fib_rec(10))
+#
+# main()
 
 
 # Sum the elements of a list recursively
+def sum(a_list):
+    # base case
+    if len(a_list) == 1:
+        return a_list[0]
+    # recursion
+    return a_list[0] + sum(a_list[1:]) # add the first number, to the sum of the rest of the list
+
+def main():
+    print(sum([1, 2, 3, 4]))
+
+main()
 # For two integers, x and y, compute x^y recursively.
 # Print out a list of elements recursively
 # Calculate whether a string is a palindrome recursively
