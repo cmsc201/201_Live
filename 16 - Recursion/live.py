5e04e474f875a222f4d7ab6f4dc9639f7d6a2398
Ben <doc.ben.johnson@gmail.com>
2019-04-10 15:02:07 -0400

    new prompts

--- 16 - Recursion/live.py
+++ 16 - Recursion/live.py
@@ -26,5 +26,8 @@ def main():
 
 main()
 
+
+# Sum the elements of a list recursively
+# For two integers, x and y, compute x^y recursively.
 # Print out a list of elements recursively
 # Calculate whether a string is a palindrome recursively
