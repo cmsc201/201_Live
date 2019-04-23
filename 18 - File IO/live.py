97a31cb51d2a05e78d6e49e2e5644481f790754f
Ben <doc.ben.johnson@gmail.com>
2019-04-23 12:32:11 -0400

    file io

--- 18 - File IO/live.py
+++ 18 - File IO/live.py
@@ -1,3 +1,57 @@
 # read in a file, and count how many functions are defined there
+# def number_functions(filename):
+#     file = open(filename, 'r')
+#     contents = file.read()
+#     file.close()
+#
+#     function_count = 0
+#     for i in range(len(contents)):
+#         if i + 4 < len(contents) and contents[i:i + 4] == "def ":
+#             function_count += 1
+#
+#     return function_count
+#
+#
+# def main():
+#     print(number_functions("live.py"))
+
+
+# main()
 # read in a file, find each time it says dogs and replace it with cats, write out the file
+# def justice(filename, outputFilename):
+#     file = open(filename, 'r')
+#     contents = file.read()
+#     file.close()
+#     contents = contents.lower()
+#     output = ""
+#     i = 0
+#     while i < len(contents):
+#         if i + 4 < len(contents) and contents[i:i + 4] == "dogs":
+#             output += "cats"
+#             i += 4
+#         else:
+#             output += contents[i]
+#             i += 1
+#
+#     # write output to a file
+#     out_file = open(outputFilename, 'w')
+#     out_file.write(output)
+#     out_file.close()
+#
+# def main():
+#     print(justice("dogfest.txt", "catfest.txt"))
+#
+# main()
 # Read in a file of integers and print their average
+def main():
+    file = open("someints.txt", 'r')
+    lines = file.readlines()
+    file.close()
+    sum = 0
+    number = len(lines)
+    for i in range(len(lines)):
+        sum += int(lines[i])
+    print(sum / number)
+
+
+main()
