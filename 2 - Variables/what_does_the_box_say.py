36acdc55ed036f516d1b951538cab79f0caa8091
Ben <doc.ben.johnson@gmail.com>
2019-09-05 16:11:06 -0400

    init

--- 2 - Variables/what_does_the_box_say.py
+++ 2 - Variables/what_does_the_box_say.py
@@ -1,4 +1,19 @@
 a = 15
 b = a
 a = 10
+b = a
+b = "Hello!"
 print(b)
+print(a)
+
+
+
+num_cats = int(input("Hey!  How many cats?"))
+
+print("You want", num_cats, "cats.  That's fantastic!  Five times that many is", 5 * num_cats)
+
+side_length = input("Enter the length of a side of a square:")
+side_length = int(side_length)
+area = side_length * side_length
+print("The area of the square is:", area)
+
