7fb84ade00bb65974f6d7c5c47246d0878cca7b0
Ben <doc.ben.johnson@gmail.com>
2019-09-11 21:51:52 -0400

    weather fashion

--- 4 - Conditionals/weather.py
+++ 4 - Conditionals/weather.py
@@ -1,5 +1,27 @@
-def main():
+if __name__ == '__main__':
     is_hot = input("Hi!  Is it hot out?")
+    # if it's hot, ask them about whether it's raining
+    if is_hot == "yes":
+        is_raining = input("it rainin?")
+        # if it's hot and raining, tell them to wear a poncho
+        if is_raining == "yes":
+            print("wear a poncho!")
+        # otherwise, tell them to wear shorts
+        else:
+            print("wear shorts!")
+    # if it's not hot ask them about the wind
+    else:
+        is_wind = input("Hey.  Hey.  Wind?")
+        # if it's not windy ask them about rain
+        if is_wind != "yes":
+            is_raining = input("It wet out?")
+            # if it's not rainy have them wear a parka
+            if is_raining != "yes":
+                print("Parka time!")
+            else:
+            # if it's cold and rainy, tell them to wear a sweateroncho
+                print("Sweateroncho is for you.")
+        # if it's cool and calm, wear a fashionable clothing situation
+        else:
+            print("Stay warm!")
 
-
-main()
