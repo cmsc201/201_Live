b64357a55db1a23048b744b7dd472c59c79e872a
Ben <doc.ben.johnson@gmail.com>
2019-09-16 14:33:15 -0400

    prompt + main

---
+++ 5 - Lists/live.py
@@ -0,0 +1,6 @@
+# A user must guess my favorite movie animals
+# A user can input grocery items and then check them off by inputting them again
+# A user inputs a series of numbers and then we print the list of numbers with their average
+
+if __name__ == '__main__':
+
