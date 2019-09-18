2fb87317de6c2b0ac3f4074542f8a55357d62ac4
Ben <doc.ben.johnson@gmail.com>
2019-09-18 13:58:36 -0400

    for updates

---
+++ 6 - For loops/for-each-live.py
@@ -0,0 +1,9 @@
+# print all the contents of a list of animals
+# print all the animals doing everything in a list of actions
+# compute the sum of a list of integers
+# print double each element in the list of integer
+# update a list to increment each integer by one
+
+if __name__ == '__main__':
+    animals = []
+
---
+++ 6 - For loops/for-i-live.py
@@ -0,0 +1,3 @@
+# How can we print the even numbers between two integers x and y with a for loop?
+# How can we use range to go backwards through a list?
+# Print all the elements of a list with their index!  How do?
