85a4f0b4bbbb63fe9c1d2832909f0f187f451a1f
Ben <doc.ben.johnson@gmail.com>
2019-09-18 13:46:11 -0400

    lists!

--- 5 - Lists/live.py
+++ 5 - Lists/live.py
@@ -41,5 +41,3 @@ if __name__ == '__main__':
     else:
         print("I don't have that on the list.")
 
-    # A user inputs a series of numbers and then we print the list of numbers with their average
-
