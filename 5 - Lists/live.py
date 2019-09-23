841d121a96d8ff8d1113892b8da6a646024810ac
Ben <doc.ben.johnson@gmail.com>
2019-09-23 16:10:58 -0400

    loops 1

--- 5 - Lists/live.py
+++ 5 - Lists/live.py
@@ -3,4 +3,12 @@
 # A user inputs a series of numbers and then we print the list of numbers with their average
 
 if __name__ == '__main__':
+    chameleon_color = "navy blue"
+    tail_coiled_factor = 0.1
 
+    if chameleon_color == "green" and tail_coiled_factor < 0.4:
+        print("I'm chillin")
+    elif tail_coiled_factor < 0.4:
+        print("I'm fancy!")
+    else:
+        print("I'm hiding!")
--- 6 - For loops/for-each-live.py
+++ 6 - For loops/for-each-live.py
@@ -5,5 +5,33 @@
 # update a list to increment each integer by one
 
 if __name__ == '__main__':
-    animals = []
+    animals = ["cat", "dog", "chinchilla", "monkey", "ferret", "road runner", "aardvark", "Professor Benjamin Johnson, Ph.D"]
 
+    for animal in animals:
+        if animal == "chinchilla":
+            print("I'm ready for my dust bath!")
+        elif animal == "road runner":
+            for slow_animal in animals:
+                if slow_animal != animal:
+                    print(animal, "is faster than", slow_animal)
+        else:
+            print(animal, "is awesome!")
+
+    # cat is awesome!
+    # dog is awesome!
+    # I'm ready for my dust bath!
+    # monkey is awesome!
+    # ferret is awesome!
+    # road runner is faster than cat!
+    # road runner is faster than dog!
+    # road runner is faster than chinchilla!
+    # road runner is faster than monkey!
+    # road runner is faster than aardvark!
+    # road runner is faster than Professor Benjamin Johnson, Ph.D!
+    # aardvark is awesome!
+    # Professor Benjamin Johnson, Ph.D is awesome!
+
+    numbers = [1, 2, 3, 4]
+    for number in numbers:
+        number += 1
+    print(numbers)
