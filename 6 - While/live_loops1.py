35d195ccffe60d808f520b39c79c1b1a8ad919a5
Ben <doc.ben.johnson@gmail.com>
2019-09-26 22:08:14 -0400

    say that name

--- 6 - While/live_loops1.py
+++ 6 - While/live_loops1.py
@@ -27,17 +27,17 @@ if __name__ == '__main__':
     #     name = input("That's right.  Cower.")
     #
     # print("YOU HAVE DEFIED ME FOR THE LAST TIME!")
-
-    name = input("Say my name.  I dare you.")
-    times_names_said = 0
-    while name == "beetlejuice" and times_names_said < 3:
-        times_names_said += 1
-        if times_names_said < 3:
-            input("That's me.  What up?")
-        else:
-            print("alright, I'm already here!")
-
-    print("Yeah, bye.")
+    #
+    # name = input("Say my name.  I dare you.")
+    # times_names_said = 0
+    # while name == "beetlejuice" and times_names_said < 3:
+    #     times_names_said += 1
+    #     if times_names_said < 3:
+    #         input("That's me.  What up?")
+    #     else:
+    #         print("alright, I'm already here!")
+    #
+    # print("Yeah, bye.")
 
     # Please enter a number to help me feel better about myself.  Guess wisely. 68
     # Oh bother.  That's not want I wanted.
@@ -49,3 +49,46 @@ if __name__ == '__main__':
 
     # add numbers till the user puts in 10
     # do this next class!!!!!!
+
+
+    # print even numbers from 1 to 10 (inclusively)
+    # for i in range(1, 11):
+    #     if i % 2 == 0:
+    #         print(i)
+
+#     Make the user input values until they guess 4.
+#     number = int(input("Guess a number!"))
+#     while number != 4:
+#         number = int(input("Hahahaha! You didn't guess the number!  You're pretty much the dumb!  I'm Great!  and very humble.  Guess again!"))
+#
+#     print("You go it")
+
+    # name = input("Hey.  Hey.  You know who I am?")
+    # while name != "beetlejuice":
+    #     print("I said, DO YOU KNOW WHO I AM?") # goes infinite
+    #
+    # print("DAS RIGHT!")
+
+    # what is my name? beetlejuice
+    # yeah.  what is my name?  beetlejuice
+    # yeah.  what is my name?  beetlejuice
+    # I'M ALREADY HERE!
+
+    # what is my name? simba
+    #  what?  no.  get out of here
+
+    name = input("Hey.  Hey.  You know who I am?")
+    times_name_said = 0
+    while name == "beetlejuice" and times_name_said < 2:
+        times_name_said += 1
+        name = input("I said, DO YOU KNOW WHO I AM?") # goes infinite
+
+    if times_name_said == 2:
+        print("I'M ALREADY HERE!")
+    else:
+        print("What?  Who?  Get outta here!")
+
+# Add numbers until the user puts in 0.
+# Compute 10!
+# Print even numbers between 0 and 100
+
