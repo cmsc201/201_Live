de77cf79eb3269e335f437d2e078b4a195a8806b
Ben <doc.ben.johnson@gmail.com>
2019-10-11 13:08:37 -0400

    strings

--- 10 - Strings/live.py
+++ 10 - Strings/live.py
@@ -1,5 +1,14 @@
 if __name__ == '__main__':
     # print the first three words of a string that the user input
+    fact = "Dr. Johnson probably needs a nap."
+    output = ''
 
+    for letter in fact:
+        if letter != 'o':
+            output += letter
+
+    fact = output
+
+    print(fact)
 
 
