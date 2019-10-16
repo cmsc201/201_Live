af84cb8e6068f09959a889db367e1c67ee82c0e9
Ben <doc.ben.johnson@gmail.com>
2019-10-16 15:10:41 -0400

    funct1

--- 10 - Functions/live.py
+++ 10 - Functions/live.py
@@ -1,6 +1,42 @@
 # Write a program that:
 # 1. Asks the user whether they want to (1) add (2) subtract or (3) both
 # 2. Write a function for add and subtract
+ADD = 1
+SUBTRACT = 2
+BOTH = 3
+
+def both(first_num, second_num):
+    add(first_num, second_num)
+    subtract(first_num, second_num)
+
+def add(huffelpufferinoington, number2):
+    print(huffelpufferinoington, "+", number2, "=", huffelpufferinoington + number2)
+
+def subtract(number1, number2):
+    print(number1, "-", number2, "=", number1 - number2)
+
+
+def print_list(some_list):
+    for item in some_list:
+        print(item)
+
+
+if __name__ == '__main__':
+    puppy_list = ["charlie"]
+    print_list(puppy_list)
+    action = int(input("Do you want to (1) add (2) subtract or (3) both?"))
+    first_num = int(input("NUMBER!"))
+    second_num = int(input("NUMBER!"))
+
+    if action == ADD:
+        add(first_num, second_num)
+    elif action == SUBTRACT:
+        subtract(first_num, second_num)
+    elif action == BOTH:
+        both(first_num, second_num)
+    else:
+        print("whaaaaaat?")
+
 
 
 # Ask a user for a number, and then offer to square it, square root it, or multiply by pi.
