1a56e4bb98266e7e81f42b642e5e6bf16562342d
Ben <doc.ben.johnson@gmail.com>
2019-10-21 17:17:19 -0400

    bad -> good

--- 12 - Program Design/badness.py
+++ 12 - Program Design/badness.py
@@ -1,25 +1,30 @@
-def main():
-    age = int(input("What is your age? "))
-    while age < 0 or age > 150:
-        age = int(input("What is your age? "))
+def get_value_in_range(prompt, minimum, maximum):
+    """
+    Asks the user for input in a range until they provide a reasonable value
+    :param prompt: what we prompt the user with
+    :param min: the minimum accepted value
+    :param max: the max accepted value
+    :return: Return the value the user inputted
+    """
+    user_input = int(input(prompt))
+    while user_input < minimum or user_input > maximum:
+        user_input = int(input(prompt))
+    return user_input
 
-    credits = int(input("How many credits have your earned toward graduation (120 if done)?"))
-    while credits < 0 or credits > 120:
-        credits = int(input("How many credits have your earned toward graduation (120 if done)?"))
 
-    arts_and_humanities_courses = int(input("How many of your two required AH courses have you taken?"))
-    while arts_and_humanities_courses < 0 or arts_and_humanities_courses > 2:
-        arts_and_humanities_courses = int(input("How many of your two required AH courses have you taken?"))
+if __name__ == '__main__':
+
+    age = get_value_in_range("What is your age?", 0, 150)
+
+    credits = get_value_in_range("How many credits have your earned toward graduation (120 if done)?", 0, 120)
+
+    arts_and_humanities_courses = get_value_in_range("How many of your two required AH courses have you taken?", 0, 2)
 
     dnd_stats = ["strength", "dexterity", "charisma", "constitution", "wisdom", "intelligence"]
     user_stats = []
     index = 0
     while index < len(dnd_stats):
         stat = dnd_stats[index]
-        user_stat = int(input("What would your " + stat + " be if you were a dungeons and dragons character?"))
-        while user_stat < 3 or user_stat > 18:
-            user_stat = int(input("What would your " + stat + " be if you were a dungeons and dragons character?"))
+        user_stat = get_value_in_range("What would your " + stat + " be if you were a dungeons and dragons character?", 3, 18)
         user_stats.append(user_stat)
         index += 1
-
-main()
