1f650a6d46016e9edbdb5be23bb231b45bf323cc
Ben <doc.ben.johnson@gmail.com>
2019-10-23 16:26:59 -0400

    bad -> good

--- 12 - Program Design/badness.py
+++ 12 - Program Design/badness.py
@@ -1,25 +1,20 @@
-def main():
-    age = int(input("What is your age? "))
-    while age < 0 or age > 150:
-        age = int(input("What is your age? "))
+def get_value_in_range_from_user(prompt, min, max):
+    value = int(input(prompt))
+    while value < min or value > max:
+        value = int(input(prompt))
+    return value
 
-    credits = int(input("How many credits have your earned toward graduation (120 if done)?"))
-    while credits < 0 or credits > 120:
-        credits = int(input("How many credits have your earned toward graduation (120 if done)?"))
+if __name__ == '__main__':
+    age = get_value_in_range_from_user("What is your age?", 0, 150)
 
-    arts_and_humanities_courses = int(input("How many of your two required AH courses have you taken?"))
-    while arts_and_humanities_courses < 0 or arts_and_humanities_courses > 2:
-        arts_and_humanities_courses = int(input("How many of your two required AH courses have you taken?"))
+    credits = get_value_in_range_from_user(
+        "How many credits have your earned toward graduation (120 if done)?"), 0, 120
 
-    dnd_stats = ["strength", "dexterity", "charisma", "constitution", "wisdom", "intelligence"]
+    arts_and_humanities_courses = get_value_in_range_from_user("How many of your two required AH courses have you taken?", 0, 2)
+
+    dnd_stats = ["strength", "dexterity", "charisma", "constitution", "wisdom",
+                 "intelligence"]
     user_stats = []
-    index = 0
-    while index < len(dnd_stats):
-        stat = dnd_stats[index]
-        user_stat = int(input("What would your " + stat + " be if you were a dungeons and dragons character?"))
-        while user_stat < 3 or user_stat > 18:
-            user_stat = int(input("What would your " + stat + " be if you were a dungeons and dragons character?"))
+    for stat in dnd_stats:
+        user_stat = get_value_in_range_from_user("What would your " + stat + " be if you were a dungeons and dragons character?", 3, 18)
         user_stats.append(user_stat)
-        index += 1
-
-main()
