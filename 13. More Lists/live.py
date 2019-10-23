f5008717457f97b1cfd2d0935118f6561e8eec91
Ben <doc.ben.johnson@gmail.com>
2019-10-23 17:21:55 -0400

    2d lists!

--- 13. More Lists/live.py
+++ 13. More Lists/live.py
@@ -1,18 +1,48 @@
-matrix = [[1, 2, 3, 4],
-          [5, 6, 7, 8],
-          [9, 1, 2, 3],
-          [4, 5, 6, 7]]
-
 
 def create_new_2d_list(height, width):
-    pass
+    output_list = []
+    for i in range(height):
+        row = []
+        for j in range(width):
+            row.append(0)
+        output_list.append(row)
+    return output_list
 
 
-def main():
-    # how do I get at the 9?
-    # how do I loop through the third row?
-    # how do I loop through the second column?
-    pass
+def my_append(matrix):
+    matrix.append("what?")
 
 
-main()
+if __name__ == '__main__':
+    matrix = [[1, 2, 3, 4],
+              [5, 6, 7, 8],
+              [9, 1, 2, 3],
+              [4, 5, 6, 7],
+              [1, 2, 3, 4]]
+
+    my_append(matrix)
+    print(matrix)
+    # # how do I get at the 9?
+    # print(matrix[2][0])
+    # # how do I loop through the third row?
+    # print("Print the third row")
+    # for thing in matrix[2]:
+    #     print(thing)
+    # # how do I loop through the second column?
+    # print("Print second column")
+    # for row in matrix:
+    #     print(row[1])
+    #
+    # chess_board = create_new_2d_list(8, 8)
+    # print(chess_board)
+
+    # matrix_copy = matrix
+    # matrix_copy = []
+    # for row in matrix:
+    #     new_row = []
+    #     for col in row:
+    #         new_row.append(col)
+    #     matrix_copy.append(new_row)
+    # matrix.append([5, 6, 7])
+    # print(matrix_copy)
+
