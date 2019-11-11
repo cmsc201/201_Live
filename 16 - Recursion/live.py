f1f897e101583ad829e55181cf5926e5ff486768
Ben <doc.ben.johnson@gmail.com>
2019-11-11 14:55:24 -0500

    live

--- 16 - Recursion/live.py
+++ 16 - Recursion/live.py
@@ -2,3 +2,18 @@
 # For two integers, x and y, compute xy recursively.
 # Print out a list of elements recursively
 # Calculate whether a string is a palindrome recursively
+
+
+def fact(n):
+    if n == 0:
+        print("I'm the base case!  The answer is 1!")
+        return 1
+    else:
+        clone_answer = fact(n - 1)
+        print(clone_answer)
+        return clone_answer * n
+
+
+
+if __name__ == '__main__':
+    print(fact(5))
